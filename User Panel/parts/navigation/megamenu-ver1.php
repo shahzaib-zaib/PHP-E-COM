8bd393d3356c15e2f6347476d3c4bccf2d766540
Shahzaib <shahzaibux@gmail.com>
2020-06-16 18:10:16 +0500

    Project Initiaied

---
+++ User Panel/parts/navigation/megamenu-ver1.php
@@ -0,0 +1,59 @@
+<div class="row mega-menu-ver1">
+	<div class="col-xs-12 col-lg-6 col-sm-6 col-md-4">
+         <div class="this-week-products">
+         	<h5 class="title uppercase">this week</h5>
+         	<ul class="products clearfix">
+         		<li><a href="index.php?page=product-simple"><img src="assets/images/products/mega1.jpg" alt=""></a>
+         			<span class="sub-title">Just Arrived</span>
+         		</li>
+         		<li class="hidden-sm hidden-md"><a href="index.php?page=product-simple"><img src="assets/images/products/mega2.jpg" alt=""></a>
+         			<span class="sub-title">Shop Handbags</span>
+         		</li>        	
+         	</ul>
+         </div>  
+	</div>
+
+	<div class="col-xs-12 col-lg-3 col-sm-3 col-md-4">
+		<ul class="sub-menu">
+			<li class="highlight"><a href="index.php?page=categories-grid">just arrived</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">new season</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">trends</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">holiday shop</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">clothing</a></li>
+			<li><a href="index.php?page=categories-grid">Dresses</a></li>
+			<li><a href="index.php?page=categories-grid">Tshirts</a></li>
+			<li><a href="index.php?page=categories-grid">Tops</a></li>
+			<li><a href="index.php?page=categories-grid">Shirts</a></li>
+			<li><a href="index.php?page=categories-grid">Shorts</a></li>
+			<li><a href="index.php?page=categories-grid">Suits</a></li>
+			<li><a href="index.php?page=categories-grid">Hoodies/Sweatshirts</a></li>
+			<li><a href="index.php?page=categories-grid">Swimwear</a></li>
+			<li><a href="index.php?page=categories-grid">Cardigans</a></li>
+			<li><a href="index.php?page=categories-grid">Jeans</a></li>
+			<li><a href="index.php?page=categories-grid">Leggins</a></li>
+			<li><a href="index.php?page=categories-grid">Underwear</a></li>
+			<li><a href="index.php?page=categories-grid">Waist Coast</a></li>
+		</ul>
+
+	</div>
+	<div class="col-xs-12 col-lg-3 col-sm-3 col-md-4">
+		<ul class="sub-menu">
+			<li class="highlight"><a href="index.php?page=categories-grid">accessories</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">bags</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">gifts</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">jewellery</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">sunglasses</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">watches</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">shoes</a></li>
+			<li><a href="index.php?page=categories-grid">Formal Shoes</a></li>
+			<li><a href="index.php?page=categories-grid">Trainers</a></li>
+			<li><a href="index.php?page=categories-grid">Boots</a></li>
+			<li><a href="index.php?page=categories-grid">Plimsolls</a></li>
+			<li><a href="index.php?page=categories-grid">Desert Boots</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">Seasonal offers</a></li>
+			<li class="highlight red"><a href="index.php?page=categories-grid">sale</a></li>
+			<li class="highlight"><a href="index.php?page=categories-grid">teen boys</a></li>
+			
+		</ul>
+	</div>
+</div>
---
+++ User Panel/parts/navigation/megamenu-ver2.php
@@ -0,0 +1,59 @@
+<div class="row mega-menu-ver2">
+	<div class="col-xs-12 col-md-3 col-sm-3">
+		<h5 class="menu-title">collection</h5>
+		<ul class="sub-menu">
+			<li><a href="index.php?page=categories-grid">Coats</a></li>
+			<li><a href="index.php?page=categories-grid">Jackets</a></li>
+			<li><a href="index.php?page=categories-grid">Jumpsuits</a></li>
+			<li><a href="index.php?page=categories-grid">Dresses</a></li>
+			<li><a href="index.php?page=categories-grid">Trousers/Skirts</a></li>
+			<li><a href="index.php?page=categories-grid">Shorts</a></li>
+			<li><a href="index.php?page=categories-grid">Jeans</a></li>
+			<li><a href="index.php?page=categories-grid">knitwear</a></li>
+			<li><a href="index.php?page=categories-grid">Sweatshirts</a></li>
+			<li><a href="index.php?page=categories-grid">Blouses / Shirts</a></li>
+			<li><a href="index.php?page=categories-grid">T-Shirts</a></li>
+			
+		</ul>
+
+	</div>
+	<div class="col-xs-12 col-md-3 col-sm-3">
+		<h5 class="menu-title">accessories</h5>
+		<ul class="sub-menu">
+			<li><a href="index.php?page=categories-grid">Bags & Backpacks</a></li>
+			<li><a href="index.php?page=categories-grid">Scarves & Foulards</a></li>
+			<li><a href="index.php?page=categories-grid">Knitted Hats & Gloves</a></li>
+			<li><a href="index.php?page=categories-grid">Tights & Socks</a></li>
+			<li><a href="index.php?page=categories-grid">Jewerly</a></li>
+			<li><a href="index.php?page=categories-grid">Accessories</a></li>
+			<li><a href="index.php?page=categories-grid">Footwear</a></li>
+			
+		</ul>
+	</div>
+	<div class="col-ms-6 col-sm-6 banners col-xs-12">
+		<div class="wide-banner cnt-strip">
+			<a href="index.php?page=catalog">
+				<div class="image">
+					<img src="assets/images/blank.gif" data-echo="assets/images/products/mega3.jpg" alt="">
+				</div>	
+				<div class="strip text-center">
+					<h5>get up to</h5>
+					<h2>-50% off</h2>
+				</div>
+			</a>
+		</div><!-- /.wide-banner-->
+
+		<div class="wide-banner cnt-strip">
+			<a href="index.php?page=catalog">
+				<div class="image">
+					<img src="assets/images/blank.gif" data-echo="assets/images/products/mega4.jpg" alt="">
+				</div>	
+				<div class="strip text-center">
+					<h5>check out</h5>
+					<h3>new arrivals</h3>
+				</div>
+			</a>
+		</div><!-- /.wide-banner-->
+
+	</div>
+</div>
