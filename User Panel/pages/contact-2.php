409c7808102e022e1c10f216f4755a6d2d15be11
Shahzaib <shahzaibux@gmail.com>
2020-06-16 17:23:51 +0500

    Project Initiaied

---
+++ User Panel/pages/contact-2.php
@@ -0,0 +1,70 @@
+
+<div class="page-header">
+	<div class="container">
+		<div class="contact-info-icons row">
+			<div class="col-md-4 col-xs-12 col-sm-6">
+				<div class="contact-info">
+					<i class="icon icon-phone-icon"></i>
+					<span class="contact-info-title">+48 655 555 555</span><br/>
+					<span class="contact-info-subtitle">Phone Us</span>
+				</div><!-- /.contact-info -->
+			</div>
+			<div class="col-md-4 col-xs-12 col-sm-6">
+				<div class="contact-info middle">
+					<i class="icon icon-mailicon"></i>
+					<span class="contact-info-title">info@bewear.com</span><br/>
+					<span class="contact-info-subtitle">Write us Email</span>
+				</div><!-- /.contact-info -->
+			</div>
+			<div class="col-md-4 col-xs-12 hidden-sm">
+				<div class="contact-info pull-right last">
+					<i class="icon icon-faxicon"></i>
+					<span class="contact-info-title">+48 655 555 555</span><br/>
+					<span class="contact-info-subtitle">FAX Us</span>
+				</div><!-- /.contact-info -->
+			</div>
+		</div><!-- /.contact-info-icons -->
+	</div><!-- /.container -->
+</div><!-- /.page-header -->
+<section class="container contact-form contact-form-2 wow fadeIn">
+	<header class="text-center">
+		<h2 class="section-title">Contact Us</h2>
+		<span class="section-subtitle">What can we help you with? Please use the form below</span>
+	</header>
+	<form class="row">
+		<div class="col-md-6">
+			<div class="form-group">
+				<label for="name">Name</label>
+				<input type="text" class="form-control" id="name">
+			</div>
+			<div class="form-group">
+				<label for="email">Email Address</label>
+				<input type="email" class="form-control" id="email">
+			</div>
+			<div class="form-group">
+				<label for="subject">Subject</label>
+				<input type="text" class="form-control" id="subject">
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="form-group">
+				<label for="message">Message</label>
+				<textarea rows="10" cols="10" class="form-control" id="message"></textarea>
+			</div>
+			<div class="form-actions">
+				<button type="submit" class="btn btn-primary btn-send-message-2">Send your message</button>	
+			</div>
+		</div>
+	</form>
+</section>
+
+<section class="map contact-2 wow fadeIn">
+	<address class="text-center light-bg">
+		<span class="address-title">Bewear Co.</span>
+		3100 West Cary Street Richmond, Virginia 23221<br>
+		Phone: +48 548 499 999
+	</address>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75448.22842567014!2d20.465698800000002!3d53.7760107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e2792d071419c7%3A0x106a8126cf54c20f!2sOlsztyn%2C+Poland!5e0!3m2!1sen!2sin!4v1426167760600" ></iframe>
+</section>
+
+<?php require ROOT . '/parts/general/newsletter-block.php'; ?>
---
+++ User Panel/pages/home-2.php
@@ -0,0 +1,114 @@
+<div class="main-content home-2">   
+	
+	<?php require ROOT . '/parts/sliders/home-2-hero-slider.php'; ?>
+	
+	<div class="container">
+		<section class="home-2-features inner-top-xs">
+			<div class="row">
+				<div class="col-md-6 col-sm-12  feature-block">
+					<a href="index.php?page=categories-grid" class="media features">
+						<div class="media-left">
+							<div class="image">
+								<img width="300" height="300" class="lazy-load media-object img-responsive" src="assets/images/blank.gif" data-echo="assets/images/products/feature1.jpg" alt="">
+							</div>
+						</div>
+						<div class="media-body media-middle">
+							<div class="section text-center">
+								<h5 class="name">john williams</h5>
+								<h3 class="tagline">perfection</h3>
+							</div>
+						</div>
+					</a><!-- /.media -->
+
+					<a href="index.php?page=categories-grid" class="media features">
+						<div class="media-body media-middle">
+							<div class="section text-center">
+								<h5 class="name">sunglasses</h5>
+								<h3 class="tagline">summer 2015</h3>
+							</div>
+						</div>
+						<div class="media-right">
+							<div class="image">
+								<img width="300" height="300" class="lazy-load media-object img-responsive" src="assets/images/blank.gif" data-echo="assets/images/products/feature2.jpg" alt="">
+							</div>
+						</div>
+					</a><!-- /.media -->
+				</div><!-- /.feature-block -->
+
+				<div class="col-md-6 col-sm-12 feature-block big-image hidden-sm">
+					<a href="index.php?page=categories-grid">
+						<img width="570" height="600" class="img-responsive lazy-load" src="assets/images/blank.gif" data-echo="assets/images/products/feature3.jpg" alt="">
+						<div class="centered-caption">
+							<div class="banner-text text-center">
+								<h4 class="banner-title">clothing</h4>
+								<h2 class="tagline">sharp styles</h2>
+							</div>
+						</div>
+					</a>
+				</div><!-- /.feature-block -->
+			</div><!-- /.row -->
+		</section><!-- /.home-2-features -->
+
+		<?php require ROOT . '/parts/section/home-2/product-tab.php'; ?>
+
+	</div><!-- /.container -->
+
+	<div class="load-more-holder text-center">
+		<a href="index.php?page=categories-grid" class="btn btn-default load-more">Load More Products</a> 
+	</div>
+	
+	<section class="wide-banners clearfix wow fadeIn">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-4">
+					<div class="wide-banner cnt-strip">
+						<a href="index.php?page=catalog">
+							<div class="image lazy-load">
+								<img width="370" height="245" src="assets/images/blank.gif" data-echo="assets/images/brands/w2.jpg" class="img-responsive" alt="">
+							</div>	
+							<div class="strip text-center">
+								<h5>over 40s</h5>
+								<h3>free delivery</h3>
+							</div>
+						</a>
+					</div><!-- /.wide-banner-->
+				</div><!-- /.col -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="wide-banner cnt-strip">
+						<a href="index.php?page=catalog">
+							<div class="image lazy-load">
+								<img width="370" height="245" src="assets/images/blank.gif" data-echo="assets/images/brands/w1.jpg" class="img-responsive" alt="">
+							</div>	
+							<div class="strip text-center">
+								<div class="strip-inner">
+									<h5>get up to</h5>
+									<h1>40% off</h1>
+								</div>	
+							</div>
+						</a>
+					</div><!-- /.wide-banner-->
+				</div><!-- /.col -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="wide-banner cnt-strip">
+						<a href="index.php?page=catalog">
+							<div class="image lazy-load">
+								<img width="370" height="245" src="assets/images/blank.gif" data-echo="assets/images/brands/w3.jpg" class="img-responsive" alt="">
+							</div>	
+							<div class="strip text-center">
+								<div class="strip-inner">
+									<h5>we care</h5>
+									<h3>world shipping</h3>
+								</div>	
+							</div>
+						</a>
+					</div><!-- /.wide-banner-->
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		</div><!-- /.container -->
+	</section><!-- /.wide-banners -->
+
+	<?php require ROOT . '/parts/general/newsletter-block.php'; ?>
+
+</div>
