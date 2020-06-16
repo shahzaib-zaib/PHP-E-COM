77a113fc2693061447ce19edc5f7109c6a32b74b
Shahzaib <shahzaibux@gmail.com>
2020-06-16 18:30:55 +0500

    Project Initiaied

---
+++ User Panel/parts/section/product-simple/product-gallery.php
@@ -0,0 +1,68 @@
+<section class="slider">
+	<div class="row">
+		<div class="col-md-10">
+			<div id="slider" class="flexslider">
+				<ul class="slides">
+					<li>
+						<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						     <img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						
+						<div class="zoom-overlay"></div>
+						</a>
+						
+					</li>
+					<li>
+						<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<div class="zoom-overlay"></div>
+					</a>
+						
+					</li>
+					<li>
+						<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<div class="zoom-overlay"></div>
+					</a>
+					
+					</li>
+					<li>
+						<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<div class="zoom-overlay"></div>
+					</a>
+						
+					</li>
+					<li>
+						<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<div class="zoom-overlay"></div>
+					</a>
+					</li>
+					
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<div id="carousel" class="flexslider">
+				<ul class="slides">
+					<li>
+						<img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" alt=""  />
+					</li>
+					<li>
+						<img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" alt=""   />
+					</li>
+					<li>
+						<img src="assets/images/blank.gif" data-echo="assets/images/products/s3.jpg" alt=""   />
+					</li>
+					<li>
+						<img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" alt=""   />
+					</li>
+					<li>
+						<img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" alt=""  />
+					</li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
