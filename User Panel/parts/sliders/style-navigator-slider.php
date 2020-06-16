c669cb4d88696691ebbdb8f4456757457c872cd3
Shahzaib <shahzaibux@gmail.com>
2020-06-16 19:07:15 +0500

    Project Initiaied

---
+++ User Panel/parts/sliders/style-navigator-slider.php
@@ -0,0 +1,78 @@
+<div class="style-navigator-section">
+	<div class="container">
+		<div class="lookbook-slider">
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/1.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/2.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/3.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/4.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/5.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/3.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/4.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/2.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/3.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/1.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/5.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+			<div class="item">
+				<a href="index.php?page=lookbook-single-look" class="single-look">
+					<img src="assets/images/lookbook/2.jpg" alt="">
+					<span class="shop-this-look text-center">Shop this look</span>
+				</a>
+			</div><!-- .item -->
+		</div><!-- /.lookbook-slider -->
+	</div><!-- /.container -->
+</div><!-- /.style-navigator-section -->
