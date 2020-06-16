83e67215ee26e47a27e21a692a59e7cf0e9f1e45
Shahzaib <shahzaibux@gmail.com>
2020-06-16 19:05:43 +0500

    Project Initiaied

---
+++ User Panel/parts/sliders/home-2-hero-slider.php
@@ -0,0 +1,65 @@
+<!-- ============================================================= SECTION – HERO ============================================================= -->
+
+<section id="hero" class="home-2-slider wow fadeIn">
+	<div id="owl-main" class="owl-carousel">
+
+		<div class="item" style="background-image: url(assets/images/hero/home-2.jpg);">
+			<div class="container">
+				<div class="caption vertical-center text-left">
+
+					<h1 class="fadeInDown-1 title"><span class="first-letter">J</span>anuary Looks</h1>
+					<p class="fadeInDown-2 tagline">New Collection</p>
+					<div class="fadeInDown-3">
+						<a href="index.php?page=lookbook-2" class="btn btn-default btn-block view-look-button">View LookBook</a>
+					</div><!-- /.fadeIn -->
+
+				</div><!-- /.caption -->
+			</div><!-- /.container -->
+		</div><!-- /.item -->
+
+		<div class="item" style="background-image: url(assets/images/hero/home-1.jpg);">
+			<div class="container">
+				<div class="caption vertical-center text-left">
+
+					<h1 class="fadeInDown-1 title"><span class="first-letter">J</span>anuary Looks</h1>
+					<p class="fadeInDown-2 tagline">New Collection</p>
+					<div class="fadeInDown-3">
+						<a href="index.php?page=lookbook-2" class="btn btn-default btn-block view-look-button">View LookBook</a>
+					</div><!-- /.fadeIn -->
+
+				</div><!-- /.caption -->
+			</div><!-- /.container -->
+		</div><!-- /.item -->
+
+		<div class="item" style="background-image: url(assets/images/hero/home-2.jpg);">
+			<div class="container">
+				<div class="caption vertical-center text-left">
+
+					<h1 class="fadeInDown-1 title"><span class="first-letter">J</span>anuary Looks</h1>
+					<p class="fadeInDown-2 tagline">New Collection</p>
+					<div class="fadeInDown-3">
+						<a href="index.php?page=lookbook-2" class="btn btn-default btn-block view-look-button">View LookBook</a>
+					</div><!-- /.fadeIn -->
+
+				</div><!-- /.caption -->
+			</div><!-- /.container -->
+		</div><!-- /.item -->
+
+		<div class="item" style="background-image: url(assets/images/hero/home-1.jpg);">
+			<div class="container">
+				<div class="caption vertical-center text-left">
+
+					<h1 class="fadeInDown-1 title"><span class="first-letter">J</span>anuary Looks</h1>
+					<p class="fadeInDown-2 tagline">New Collection</p>
+					<div class="fadeInDown-3">
+						<a href="index.php?page=lookbook-2" class="btn btn-default btn-block view-look-button">View LookBook</a>
+					</div><!-- /.fadeIn -->
+
+				</div><!-- /.caption -->
+			</div><!-- /.container -->
+		</div><!-- /.item -->
+
+	</div><!-- /.owl-carousel -->
+</section>
+
+<!-- ============================================================= SECTION – HERO : END ============================================================= -->
---
+++ User Panel/parts/sliders/lookbook-slider.php
@@ -0,0 +1,76 @@
+<!-- ============================================================= LOOKBOOK SLIDER ============================================================= -->
+<div class="lookbook-slider">
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/1.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a>
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/2.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look active">
+			<img src="assets/images/lookbook/3.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/4.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/5.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/3.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/2.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/4.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/1.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/5.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/3.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+	<div class="item">
+		<a href="index.php?page=single-look" class="single-look">
+			<img src="assets/images/lookbook/2.jpg" alt="">
+			<span class="shop-this-look">Shop this look</span>
+		</a><!-- /.single-look -->
+	</div><!-- .item -->
+</div><!-- /.lookbook-slider -->
+<!-- ============================================================= LOOKBOOK SLIDER: END ============================================================= -->
