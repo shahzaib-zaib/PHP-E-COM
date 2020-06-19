d09d1b0d08b56ee1d29040be6745d643b3549f7c
Shahzaib <shahzaibux@gmail.com>
2020-06-19 21:41:01 +0500

    Updated Project

---
+++ User Panel/pages/about-us.php
@@ -0,0 +1,137 @@
+<section class="about-cover-image lazy-load">
+	<img src="assets/images/blank.gif" data-echo="assets/images/about/01.jpg" width="1934" height="734" class="img-responsive" alt="">
+</section>
+<section class="container about-features">
+	<header class="text-center wow fadeInUp">
+		<h2 class="section-title">Working with Fashion is Our Life</h2>
+		<span class="section-subtitle">We are a second-generation family business established in 1972</span>
+	</header>
+	<div class="row wow fadeInUp">
+		<div class="col-md-4 col-sm-4 feature">
+			<div class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/f1.jpg" alt="Feature Image" width="370" height="280" class="img-responsive feature-image"></div>
+			<h4 class="feature-title">Family of creators</h4>
+			<div class="feature-description">
+				<p>Sed vulputate elit vitae magna lacinia, vel bibendum neque faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu, pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus et ipsum rutrum dignissim. Praesent ultrices posuere eros ac tristique.</p>
+			</div>
+		</div><!-- /.feature -->
+		<div class="col-md-4 col-sm-4 feature">
+			<div class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/f2.jpg" alt="Feature Image" width="370" height="280" class="img-responsive feature-image"></div>
+			<h4 class="feature-title">Made the hard way</h4>
+			<div class="feature-description">
+				<p>Sed vulputate elit vitae magna lacinia, vel bibendum neque faucibus. Aliquam sed volutpat turpis.</p>
+				<p>Be Bold //Do Everything Better<br/> Be Open // Work Stronger Together</p>
+			</div>
+		</div><!-- /.feature -->
+		<div class="col-md-4 col-sm-4 feature">
+			<div class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/f3.jpg" alt="Feature Image" width="370" height="280" class="img-responsive feature-image"></div>
+			<h4 class="feature-title">Design Driven</h4>
+			<div class="feature-description">
+				<p>Sed vulputate elit vitae magna lacinia, vel bibendum neque faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu, pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus et ipsum rutrum dignissim. Praesent ultrices posuere eros ac tristique.</p>
+			</div>
+		</div><!-- /.feature -->
+	</div>
+</section><!-- /.features -->
+
+<section class="the-process">
+	<header class="wow fadeIn lazy-load">
+		<img src="assets/images/blank.gif" data-echo="assets/images/about/header-image.jpg" alt="Header Image" class="img-responsive lazy-load">
+	</header>
+	<div class="container process-container wow fadeInUp">
+		<div class="row">
+			<div class="col-md-4 first-column">
+				<h2 class="section-title">The Process</h2>
+			</div>
+			<div class="col-md-4 process-description second-column">
+				<p>Sed vulputate elit vitae magna lacinia, vel bibendum neque faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu, pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus et ipsum rutrum dignissim. Praesent ultrices posuere eros ac tristique.</p>
+			</div>
+			<div class="col-md-4 process-description third-column">
+				<p>Sed vulputate elit vitae magna lacinia, vel bibendum neque faucibus. Aliquam sed volutpat turpis. Phasellus nisl arcu, pretium eu faucibus sed, aliquet in lacus. Pellentesque sed lacus et ipsum rutrum dignissim. Praesent ultrices posuere eros ac tristique.</p>
+			</div>
+		</div>
+	</div><!-- /.container -->
+</section><!-- .the-process -->
+
+<section class="our-team">
+	<div class="container our-team-container">
+		<header class="text-center wow fadeIn">
+			<h2 class="section-title">Our Team</h2>
+		</header>
+		<div class="row team-members wow fadeInUp">
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t1.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t2.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t3.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t4.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t5.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+			<div class="col-md-2 col-sm-3 team-member">
+				<figure class="lazy-load">
+					<img src="assets/images/blank.gif" data-echo="assets/images/about/t6.jpg" alt="Team Member Name" class="img-responsive">
+					<figcaption>
+						<span class="team-member-name">Ashley Runion</span>
+						<span class="team-member-designation">Designer</span>
+						<span class="team-member-readmore"><a href="#">Read More</a></span>
+					</figcaption>
+				</figure>
+			</div><!-- /.team-member -->
+		</div>
+	</div><!-- /.container -->
+</section><!-- /.our-team -->
+
+<section class="container our-clients wow fadeIn">
+	<header class="text-center">
+		<h2 class="section-title">We are co-operating with</h2>
+	</header>
+	<div class="brands-container">
+		<ul class="brands">
+			<li class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/brand1.jpg" alt="Brand Name"></li>
+			<li class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/brand2.jpg" alt="Brand Name"></li>
+			<li class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/brand3.jpg" alt="Brand Name"></li>
+			<li class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/brand4.jpg" alt="Brand Name"></li>
+			<li class="lazy-load"><img src="assets/images/blank.gif" data-echo="assets/images/about/brand5.jpg" alt="Brand Name"></li>
+		</ul>
+	</div>
+</section>
