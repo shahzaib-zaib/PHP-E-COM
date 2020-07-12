095b632070b875f566fcc79fef0f9cdbe9c004a6
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:14:37 +0500

    Updated Project

---
+++ pages/categories-grid.php
@@ -0,0 +1,61 @@
+<main>
+	<section class="category-header">
+		<header>
+			<h1 class="category-title">Shop by Category</h1>
+			<span class="category-subtitle">Pick up category you are interested in</span>
+		</header>
+	</section><!-- /.catalog-header -->
+    
+    <div class="category-content">
+	   <div class="category-features">
+		    <div class="container">
+			    <div class="row">
+				    <div class="col-md-3 col-sm-6">
+					    <div class="text-center">
+						    <div class="media feature">
+							    <div class="media-left">
+								    <i class="icon icon-30-days-return"></i>
+							    </div>
+							    <div class="media-body media-middle">
+								    <h4 class="media-heading">30 days return</h4>
+							    </div>
+						    </div>
+					    </div>
+				    </div><!--col-->
+
+				    <div class="col-md-6 hidden-sm">
+					    <div class="text-center">
+						    <div class="media feature">
+							    <div class="media-left">
+								    <i class="icon icon-truck"></i>
+							    </div>
+							    <div class="media-body media-middle">
+								    <h4 class="media-heading">free shipping! on orders above $80</h4>
+							    </div>
+						    </div>
+					    </div>
+				    </div><!--col-->
+
+				    <div class="col-md-3 col-sm-6">
+					    <div class="text-center">
+						    <div class="media feature">
+							    <div class="media-left">
+								    <i class="icon icon-secure-payment"></i>
+							    </div>
+							    <div class="media-body media-middle">
+								    <h4 class="media-heading">secure payments</h4>
+							    </div>
+						    </div>
+					    </div>
+				    </div><!--col-->
+			    </div><!--row-->
+		    </div><!--container-->
+	    </div><!--category-features-->
+
+        
+    </div><!--category-content-->
+
+	<?php require ROOT . '/parts/general/newsletter-block.php'; ?>
+</main>
+
+
