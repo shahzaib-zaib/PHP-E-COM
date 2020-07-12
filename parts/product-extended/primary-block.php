6c0f56c08f8860fa76f867716ae399483c9ddacb
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:41:28 +0500

    Updated Project

--- parts/product-extended/primary-block.php
+++ parts/product-extended/primary-block.php
@@ -6,6 +6,94 @@
 					<?php require_once ROOT.'/parts/product-extended/product-gallery.php'; ?>
 				</div>
 			</div>
+            <div class="col-sm-6 col-lg-5 body-holder body-holder-style-2">
+				<div class="product-info">
+					<div class="product-rating-holder">
+						<a href="#" class="product-rating">
+							<div class="star-rating gray" title="Rated 5.00 out of 5">
+								<span style="width:80%">
+									<strong class="rating">5.00</strong>
+									out of 5
+								</span>
+							</div>
+						</a>
+						<a href="#reviews" class="review-link">(4 reviews)</a>
+					</div>
+
+					<h1 class="single-product-title">Converse Chuck Taylor</h1>
+					<div class="product-brand">Calvin Klein</div>
+					<div class="excerpt inner-vs">
+						<p>Sed quis nunc efficitur, gravida orci sed, gravida felis. Quisque non euismod felis. Suspendisse consectetur, tellus in condimentum fringilla, turpis massa facilisis augue, eget tempor nibh dui in dolor.</p>
+					</div>
+					<div class="product-attributes">
+						<div class="color-holder clearfix">
+							<span class="key">Color: </span><span class="value">Black  Native</span>
+							<ul class="color-picker clearfix">
+								<li><input class="le-radio blue checked" type="radio" value="color1" name="group" checked="checked"></li>
+								<li><input class="le-radio gray" type="radio" value="color2" name="group"></li>
+								<li><input class="le-radio red" type="radio" value="color3" name="group"></li>
+								<li><input class="le-radio green" type="radio" value="color4" name="group"></li>
+								<li><input class="le-radio" type="radio" value="color5" name="group"></li>
+							</ul>
+						</div>
+
+						<div class="size-holder m-t-20 clearfix">
+							<span class="key">Size:</span>
+							<ul class="size-picker clearfix">
+								<li><input id="group1" class="attribute-radio" type="radio" value="color1" name="group">
+									<label for="group1"><span>7</span></label>		
+								</li>
+								<li><input id="group2" class="attribute-radio" type="radio" value="color2" name="group">
+									<label for="group2"><span>8</span></label>																	
+								</li>
+								<li><input id="group3" class="attribute-radio" type="radio" value="color3" name="group">
+									<label for="group3"><span>8.5</span></label>					
+								</li>
+								<li><input id="group4" class="attribute-radio" type="radio" value="color4" name="group">
+									<label for="group4"><span>9</span></label>			
+								</li>
+								<li><input id="group5" class="attribute-radio" type="radio" value="color5" name="group">
+									<label for="group5"><span>9.5</span></label>						
+								</li>
+								<li><input id="group6" class="attribute-radio" type="radio" value="color6" name="group">
+									<label for="group6"><span>10</span></label>						
+								</li>
+								<li><input id="group7" class="attribute-radio" type="radio" value="color7" name="group">
+									<label for="group7"><span>11</span></label>						
+								</li>
+								<li><input id="group8" class="attribute-radio" type="radio" value="color8" name="group">
+									<label for="group8"><span>12.5</span></label>						
+								</li>
+								<li><input id="group9" class="attribute-radio" type="radio" value="color9" name="group">
+									<label for="group9"><span>13</span></label>						
+								</li>
+								<li><input id="group10" class="attribute-radio" type="radio" value="color10" name="group">
+									<label for="group10"><span>13.5</span></label>						
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="qnt-holder">
+						<form class="cart">
+							<div class="quantity-holder">
+								<span class="key">Qty:</span>
+								<input type="number" class="txt txt-qty" title="Qty" value="1" name="quantity" min="1" step="1">
+							</div>
+
+							<a href="#" title="Wishlist" class="btn add-to-wishlist">add to wishlist</a>
+						</form>
+					</div>
+
+					<div class="add-to-bag-holder">
+						<a href="#" class="add-bag-button btn btn-block">add to bag</a>
+						<p class="single-product-price text-center">$135.00</p>
+					</div>
+
+					<a href="#" class="sharing-with-friends text-center">share it with your friends</a>
+
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
