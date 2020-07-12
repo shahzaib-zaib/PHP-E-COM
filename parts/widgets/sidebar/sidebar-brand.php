afc9ace08e66c42b78e8c80104d0824101de6ad0
Shahzaib <shahzaibux@gmail.com>
2020-07-12 18:51:57 +0500

    Updated Project

---
+++ parts/widgets/sidebar/sidebar-brand.php
@@ -0,0 +1,22 @@
+<div class="sidebar">
+	<h5 class="sidebar-title uppercase">brand</h5>
+	<div class="body">
+		<ul>
+			<li><a href="index.php?page=product-simple">Amuse Society</a></li>
+			<li><a href="index.php?page=product-simple">Asilio</a></li>
+			<li><a href="index.php?page=product-simple">Assembly New York</a></li>
+			<li><a href="index.php?page=product-simple">AZY4UO</a></li>
+			<li><a href="index.php?page=product-simple">Band of Gypsies</a></li>
+			<li><a href="index.php?page=product-simple">BB Dakota</a></li>
+			<li><a href="index.php?page=product-simple">BDG</a></li>
+			<li><a href="index.php?page=product-simple">Blue Life</a></li>
+			<li><a href="index.php?page=product-simple">Blue Life Fit</a></li>
+			<li><a href="index.php?page=product-simple">Cameo</a></li>
+			<li><a href="index.php?page=product-simple">Cheap Monday</a></li>
+			<li><a href="index.php?page=product-simple">Chums</a></li>
+			<li><a href="index.php?page=product-simple">Cooperative</a></li>
+			<li><a href="index.php?page=product-simple">COPE</a></li>
+			<li><a href="index.php?page=product-simple">Ecote</a></li>
+		</ul>
+	</div><!-- /.body -->
+</div><!-- /.sidebar -->
