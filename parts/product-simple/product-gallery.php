ab5e0d5ec3207eb42e251070626e59ac3e9b5d7d
Shahzaib <shahzaibux@gmail.com>
2020-07-12 17:33:45 +0500

    Updated Project

---
+++ parts/product-simple/product-gallery.php
@@ -0,0 +1,38 @@
+<section class="slider wow fadeIn">
+	<div class="row">
+		<div class="col-md-10 col-xs-10">
+			<ul id="product-images">
+				<li>
+					<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<span class="zoom-overlay"></span>
+					</a>
+				</li>
+				<li>
+					<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<span class="zoom-overlay"></span>
+					</a>
+				</li>
+				<li>
+					<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<span class="zoom-overlay"></span>
+					</a>
+				</li>
+				<li>
+					<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<span class="zoom-overlay"></span>
+					</a>
+				</li>
+				<li>
+					<a href="assets/images/products/big-image.jpg" data-title="Gallery" data-lightbox="image-1">
+						<img src="assets/images/blank.gif" data-title="Gallery" data-lightbox="image-1" data-echo="assets/images/products/big-image.jpg" alt="" />
+						<span class="zoom-overlay"></span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
