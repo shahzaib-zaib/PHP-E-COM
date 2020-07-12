7408a7e73970d48a2219aeb800497b871e3d8b19
Shahzaib <shahzaibux@gmail.com>
2020-07-12 17:35:47 +0500

    Updated Project

--- parts/product-simple/product-gallery.php
+++ parts/product-simple/product-gallery.php
@@ -34,5 +34,19 @@
 				</li>
 			</ul>
 		</div>
+        <div class="col-md-2 col-xs-2">
+			<ul id="product-thumbnails" class="vertical">
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s3.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s3.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s2.jpg" class="lazy-load" alt="" /></li>
+				<li><img src="assets/images/blank.gif" data-echo="assets/images/products/s1.jpg" class="lazy-load" alt="" /></li>
+			</ul>
+		</div>
 	</div>
 </section>
