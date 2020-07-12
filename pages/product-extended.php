dab526aa8779bca1c40d186e6db7ac664816eee7
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:23:07 +0500

    Updated Project

---
+++ pages/product-extended.php
@@ -0,0 +1,17 @@
+<main class="single-page productextended-page">
+	<div class="breadcrumb-holder">
+		<div class="container">
+			<ul class="breadcrumb pull-left">
+				<li><a href="index.php?page=home">Home</a></li>
+				<li><a href="index.php?page=categories-grid">Men</a></li>
+				<li><a href="index.php?page=catalog">New Arrivals</a></li>
+				<li class='active'>Converse Chuck Taylor</li>
+			</ul><!--breadcrumb-->
+
+			<div class="nav-links clearfix">
+				<a href="index.php?page=product-simple" class="prev"><i class="icon icon-pagination-left icon-prev"></i>Previous</a>
+				<a href="index.php?page=product-simple" class="next">Next<i class="icon icon-pagination-right icon-next"></i></a>
+			</div><!--nav-links-->
+		</div><!--container-->
+	</div><!--breadcrumb-holder-->
+</main>
