48338eb12d6f6b58657a435d2bc8f89f55f89960
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:02:06 +0500

    Updated Project

---
+++ pages/blog.php
@@ -0,0 +1,32 @@
+<div class="feature-blog-post">
+	<div class="container">
+		<div class="row">
+			<article class="post col-md-10 col-md-push-1">
+				
+				<?php require ROOT . '/parts/sliders/featured-post-slider.php'; ?>
+				
+				<header class="entry-header text-center wow fadeInUp">
+					<div class="entry-meta">
+						<span class="cat-links">
+							<span class="sr-only">Categories </span>
+							<a href="index.php?page=blog" rel="category tag">Inside</a>
+						</span><!-- /.cat-links -->
+						<span class="posted-on">
+							<span class="sr-only">Posted on </span>
+							<a href="index.php?page=blog" rel="bookmark">
+								<time class="entry-date published" datetime="2015-01-22T20:37:21+00:00">Jan 22, 2015</time>
+								<time class="updated" datetime="2015-01-25T20:43:06+00:00">Jan 25, 2014</time>
+							</a>
+						</span><!-- /.posted-on -->
+					</div><!-- /.entry-meta -->
+					<h2 class="entry-title"><a href="index.php?page=blog-post" rel="bookmark">A new year means new adventures, are you #WEAReady?</a></h2>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content text-center wow fadeInUp">
+					<a href="index.php?page=blog-post" class="btn btn-lg btn-primary btn-readmore">Read More</a>
+				</div>
+				
+			</article><!-- /.post -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</div><!-- /.feature-blog-post -->
