8ef6f1f98daa65c1ec175ca4831da9d325b05d04
Shahzaib <shahzaibux@gmail.com>
2020-07-12 17:25:15 +0500

    Updated Project

---
+++ parts/product-simple/primary-block.php
@@ -0,0 +1,32 @@
+<div id="single-product" class="inner-top-50">
+	<div class="container">
+		<div class="row single-product-row wow fadeIn">
+			<div class="col-sm-6 col-lg-6 gallery-holder">
+				<div class="product-image-slider">
+	              <?php require_once ROOT.'/parts/product-simple/product-gallery.php'; ?>
+	           </div>
+			</div>
+			<div class="col-sm-6 col-lg-6 body-holder body-holder-style-1">
+				<div class="product-info">
+					<div class="product-rating-holder">
+						<a href="index.php?page=product-extended" class="product-rating">
+							<div class="star-rating gray" title="Rated 5.00 out of 5">
+								<span style="width:80%">
+									<strong class="rating">5.00</strong>
+									out of 5
+								</span>
+							</div>
+						</a>
+						<a href="#reviews" class="review-link">(4 reviews)</a>
+					</div>
+
+					<h1 class="single-product-title">Converse Chuck Taylor</h1>
+					<div class="product-brand">Calvin Klein</div>
+					<div class="product-price">
+						<ins><span class="amount">$492.00</span></ins>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
