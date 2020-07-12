9ebefe99d69396698935ead85ef583eed5e4c0ac
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:40:08 +0500

    Updated Project

---
+++ parts/product-extended/primary-block.php
@@ -0,0 +1,11 @@
+<div id="single-product" class="inner-top-50 wow fadeIn">
+	<div class="container">
+		<div class="row single-product-row">
+			<div class="col-sm-6 col-lg-6 gallery-holder">
+				<div class="product-image-slider">
+					<?php require_once ROOT.'/parts/product-extended/product-gallery.php'; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
