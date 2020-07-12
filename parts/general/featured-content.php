cf373adb8da7f338dda71deabd1803178bd25b76
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:27:08 +0500

    Updated Project

---
+++ parts/general/featured-content.php
@@ -0,0 +1,73 @@
+<?php
+$sliders = array(
+	1 => array(
+		'full_image' => 'assets/images/products/f1.jpg',
+		'top_left' => array(
+			'image' => 'assets/images/products/1.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'top_right' => array(
+			'image' => 'assets/images/products/2.jpg',
+			'title'	=> 'Tote Handbag',
+			'price' => '$350',
+			),
+		'bottom_left' => array(
+			'image' => 'assets/images/products/3.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'bottom_right' => array(
+			'image' => 'assets/images/products/4.jpg',
+			'title'	=> 'nola sunglasses',
+			'price' => '$350',
+			),
+		),
+	2 => array(
+		'full_image' => 'assets/images/products/f2.jpg',
+		'top_left' => array(
+			'image' => 'assets/images/products/13.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'top_right' => array(
+			'image' => 'assets/images/products/14.jpg',
+			'title'	=> 'Tote Handbag',
+			'price' => '$350',
+			),
+		'bottom_left' => array(
+			'image' => 'assets/images/products/15.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'bottom_right' => array(
+			'image' => 'assets/images/products/16.jpg',
+			'title'	=> 'nola sunglasses',
+			'price' => '$350',
+			),
+		),
+	3 => array(
+		'full_image' => 'assets/images/products/f3.jpg',
+		'top_left' => array(
+			'image' => 'assets/images/products/17.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'top_right' => array(
+			'image' => 'assets/images/products/18.jpg',
+			'title'	=> 'Tote Handbag',
+			'price' => '$350',
+			),
+		'bottom_left' => array(
+			'image' => 'assets/images/products/19.jpg',
+			'title'	=> 'white fashion dres',
+			'price' => '$350',
+			),
+		'bottom_right' => array(
+			'image' => 'assets/images/products/20.jpg',
+			'title'	=> 'nola sunglasses',
+			'price' => '$350',
+			),
+		),
+	);
+?>
