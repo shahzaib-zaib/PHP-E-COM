2769f3b41aafcffea7d1dcea2e00917d1f1b7fae
Shahzaib <shahzaibux@gmail.com>
2020-07-12 17:28:10 +0500

    Updated Project

--- parts/product-simple/primary-block.php
+++ parts/product-simple/primary-block.php
@@ -34,6 +34,60 @@
 
 						</ul>
 					</div>
+                    <div class="product-attributes">
+						<div class="color-holder clearfix">
+							<span class="key">Color:</span> <span class="value">Black  Native</span>
+							<ul class="color-picker clearfix">
+								<li><input class="le-radio blue checked" type="radio" value="color1" name="main" checked="checked">		
+								</li>
+								<li><input class="le-radio gray" type="radio" value="color2" name="main">																	
+								</li>
+								<li><input class="le-radio red" type="radio" value="color3" name="main">				
+								</li>
+								<li><input class="le-radio green" type="radio" value="color4" name="main">		
+								</li>
+								<li><input class="le-radio" type="radio" value="color5" name="main">					
+								</li>
+							</ul>
+						</div>
+
+						<div class="size-holder m-t-20 clearfix">
+							<span class="key">size:</span>
+							<ul class="size-picker clearfix">
+								<li><input id="group1" class="attribute-radio" type="radio" value="color1" name="group">
+								<label for="group1"><span>7</span></label>		
+								</li>
+								<li><input id="group2" class="attribute-radio" type="radio" value="color2" name="group">
+								<label for="group2"><span>8</span></label>																	
+								</li>
+								<li><input id="group3" class="attribute-radio" type="radio" value="color3" name="group">
+								<label for="group3"><span>8.5</span></label>					
+								</li>
+								<li><input id="group4" class="attribute-radio" type="radio" value="color4" name="group">
+								<label for="group4"><span>9</span></label>			
+								</li>
+								<li><input id="group5" class="attribute-radio" type="radio" value="color5" name="group">
+								<label for="group5"><span>9.5</span></label>						
+								</li>
+								<li><input id="group6" class="attribute-radio" type="radio" value="color6" name="group">
+								<label for="group6"><span>10</span></label>						
+								</li>
+								<li><input id="group7" class="attribute-radio" type="radio" value="color7" name="group">
+								<label for="group7"><span>11</span></label>						
+								</li>
+								<li><input id="group8" class="attribute-radio" type="radio" value="color8" name="group">
+								<label for="group8"><span>12.5</span></label>						
+								</li>
+								<li><input id="group9" class="attribute-radio" type="radio" value="color9" name="group">
+								<label for="group9"><span>13</span></label>						
+								</li>
+								<li><input id="group10" class="attribute-radio" type="radio" value="color10" name="group">
+								<label for="group10"><span>13.5</span></label>						
+								</li>
+							</ul>
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</div>
