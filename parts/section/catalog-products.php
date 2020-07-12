f0a3191d6af88cbd4ef733555f90dd6f1f20be63
Shahzaib <shahzaibux@gmail.com>
2020-07-12 18:36:15 +0500

    Updated Project

--- parts/section/catalog-products.php
+++ parts/section/catalog-products.php
@@ -339,3 +339,40 @@
 		</div><!-- .product-info -->
 	</div>
 </div>
+
+<div class='col-md-6 col-sm-6 col-lg-4 col-xs-12 product-holder'>
+	<div class="product">
+
+		<div class="image">
+			<a href="index.php?page=product-simple"><img class="img-responsive" width="224" src="assets/images/catalog/8.jpg" alt=""></a>
+		</div><!-- .image -->
+		<div class="product-info m-t-20 text-center">
+			<a class="quick-view uppercase" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="icon icon-more-colors-product"></i> Quick view</a>
+			<ul class="color-picker clearfix">
+				<li class="selected"><input class="le-radio group black" type="radio" value="g1" checked="checked" name="c8" >																	
+				</li>
+				<li><input class="le-radio gray" type="radio" value="g1-2" name="c8">				
+				</li>			
+			</ul><!-- .color-picker -->
+			<h5 class="name uppercase"><a href="index.php?page=product-simple">rotterdam tshirt</a></h5>
+			<div class="product-price">
+				<ins><span class="amount">$19</span></ins>
+			</div><!-- .product-price -->
+			<div class="buttons-holder m-t-20">					
+				<div class="add-cart-holder">
+					<a title="Add to cart" href="index.php?page=checkout" class="cart-button btn btn-primary">
+						<span>Add to bag</span>
+					</a>
+
+				</div><!-- .add-cart-holder -->
+
+				<div class="add-wishlist-holder">
+
+					<a href="index.php?page=checkout" title="Wishlist" class="wishlist-button uppercase">
+						<span class="icon icon-wishlist"></span> add to wishlist
+					</a>
+				</div><!-- .add-wishlist-holder -->
+			</div><!-- .buttons-holder -->
+		</div><!-- .product-info -->
+	</div>
+</div>
