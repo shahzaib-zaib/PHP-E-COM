553d04d850332a8f1a0fb17142fc365d7007e540
Shahzaib <shahzaibux@gmail.com>
2020-07-12 18:54:07 +0500

    Updated Project

---
+++ parts/widgets/sidebar-filter.php
@@ -0,0 +1,83 @@
+<div class="sidebar">
+	<h5 class="sidebar-title uppercase">filter</h5>
+	<div class="body">
+		<div class="accordion">
+	        <div class="accordion-group">
+	            <div class="accordion-heading">
+	                <a href="#collapseOne" data-toggle="collapse" class="accordion-toggle collapsed">
+	                    Price
+	                </a>
+	            </div><!-- /.accordion-heading -->
+	            <div class="accordion-body collapse" id="collapseOne">
+	                <div class="accordion-inner">
+	                    <div id="slider-range" class="m-t-20"></div>
+						<p class="slider-price-range"><input type="text" id="amount"></p>
+	                </div><!-- /.accordion-inner -->
+	            </div><!-- /.accordion-body -->
+	        </div><!-- /.accordion-group -->
+
+	        <div class="accordion-group">
+	            <div class="accordion-heading">
+	                <a href="#collapseTwo" data-toggle="collapse" class="accordion-toggle collapsed">
+	                    Color
+	                </a>
+	            </div><!-- /.accordion-heading -->
+	            <div class="accordion-body collapse" id="collapseTwo">
+	                <div class="accordion-inner">
+                    	<div class="color-holder clearfix">							
+							<ul class="color-picker clearfix">
+								<li><input class="le-radio blue checked" type="radio" value="color1" name="sidebar" checked="checked"></li>
+								<li><input class="le-radio gray" type="radio" value="color2" name="sidebar"></li>
+								<li><input class="le-radio red" type="radio" value="color3" name="sidebar"></li>
+								<li><input class="le-radio green" type="radio" value="color4" name="sidebar"></li>
+								<li><input class="le-radio" type="radio" value="color5" name="sidebar"></li>
+							</ul><!-- /.color-picker -->
+						</div><!-- /.color-holder -->
+	                </div><!-- /.accordion-inner -->
+	            </div><!-- /.accordion-body -->
+	        </div><!-- /.accordion-group -->
+
+
+	        <div class="accordion-group">
+	            <div class="accordion-heading">
+	                <a href="#collapse3" data-toggle="collapse" class="accordion-toggle collapsed">
+	                    Size
+	                </a>
+	            </div><!-- /.accordion-heading -->
+	            <div class="accordion-body collapse" id="collapse3">
+	                <div class="accordion-inner">
+	                    <div class="size-holder clearfix">
+							<ul class="size-picker clearfix">
+								<li>
+									<input id="group1" class="attribute-radio" type="radio" value="color1" name="group">
+									<label for="group1"><span>7</span></label>
+								</li>
+								<li>
+									<input id="group2" class="attribute-radio" type="radio" value="color2" name="group">
+									<label for="group2"><span>8</span></label>																	
+								</li>
+								<li>
+									<input id="group3" class="attribute-radio" type="radio" value="color3" name="group">
+									<label for="group3"><span>8.5</span></label>					
+								</li>
+								<li>
+									<input id="group4" class="attribute-radio" type="radio" value="color4" name="group">
+									<label for="group4"><span>9</span></label>			
+								</li>
+								<li>
+									<input id="group5" class="attribute-radio" type="radio" value="color5" name="group">
+									<label for="group5"><span>9.5</span></label>						
+								</li>
+								<li>
+									<input id="group6" class="attribute-radio" type="radio" value="color6" name="group">
+									<label for="group6"><span>10</span></label>						
+								</li>
+							</ul><!-- /.size-picker -->
+						</div><!-- /.size-holder -->
+	                </div><!--- /.accordion-inner -->
+	            </div><!-- /.accordion-body -->
+
+	        </div><!-- /.accordion-group -->
+	    </div><!-- /.accordion -->
+	</div><!-- /.body -->
+</div><!-- /.sidebar -->
