c370974ad8a438233d88f7efdb90454c46224e6a
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:48:56 +0500

    Updated Project

---
+++ parts/product-extended/product-tab.php
@@ -0,0 +1,19 @@
+<div class="single-product-tab">
+	<div role="tabpanel">
+		<!-- Nav tabs -->
+		<div class="container">
+			<div class="tab-nav-holder single-product-tab inner-bottom-xs">
+				<ul class="nav nav-tabs uppercase" role="tablist">
+					<li role="presentation" class="active"><a href="#description" aria-controls="description" role="tab" data-toggle="tab">Description</a></li>
+					<li role="presentation"><a href="#sizing" aria-controls="sizing" role="tab" data-toggle="tab">Sizing</a></li>
+					<li role="presentation"><a href="#reviews" aria-controls="reviews" role="tab" data-toggle="tab">Reviews (4)</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="tab-content bewear-tab-content">
+			
+		</div><!-- /.tab-content -->
+		
+	</div><!-- /.tabpanel -->
+</div><!-- /.single-product-tab -->
