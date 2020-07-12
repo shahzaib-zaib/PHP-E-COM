98de6160edac193d713bac0ead9752ea1100aaae
Shahzaib <shahzaibux@gmail.com>
2020-07-12 18:53:02 +0500

    Updated Project

---
+++ parts/widgets/sidebar/sidebar-category.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+	<h5 class="sidebar-title uppercase">category</h5>
+	<div class="body">
+		<ul>
+			<li><a href="index.php?page=product-simple">New Arrivals</a></li>
+			<li><a href="index.php?page=product-simple">Dresses</a></li>
+			<li class="bold"><a href="index.php?page=product-simple">Tops</a></li>
+			<li><a href="index.php?page=product-simple">T-shirts</a></li>
+			<li><a href="index.php?page=product-simple">Playsuits</a></li>
+			<li><a href="index.php?page=product-simple">Swimwear</a></li>
+			<li><a href="index.php?page=product-simple">Skirts</a></li>
+			<li><a href="index.php?page=product-simple">Shorts</a></li>
+			<li><a href="index.php?page=product-simple">Jeans</a></li>
+			<li><a href="index.php?page=product-simple">Trousers</a></li>
+			<li><a href="index.php?page=product-simple">Leggings</a></li>
+			<li class="highlight"><a href="index.php?page=product-simple">Sale</a></li>
+		</ul>
+	</div><!-- /.body -->
+</div><!-- /.sidebar -->
