0906fbdcdda819164fa11ebd5a16568a7eafb043
Shahzaib <shahzaibux@gmail.com>
2020-07-12 16:29:06 +0500

    Updated Project

--- parts/general/featured-content.php
+++ parts/general/featured-content.php
@@ -71,3 +71,33 @@ $sliders = array(
 		),
 	);
 ?>
+<div class="row featured-content-holder">
+    <div class="col-md-4 col-sm-12">
+		<div class="featured-top-left">
+			<?php foreach($sliders as $id => $slide): ?>
+				<div class="featured-product text-center featured-image-<?php echo $id; ?>">
+					<div class="image lazy-load">
+						<a href="index.php?page=product-simple"><img class="img-responsive" src="assets/images/blank.gif" data-echo="<?php echo $slide['top_left']['image']; ?>" alt=""></a>
+					</div><!-- .image -->
+					<div class="product-body">
+						<h3 class="product-title"><a href="index.php?page=product-simple"><?php echo $slide['top_left']['title']; ?></a></h3>
+						<p class="product-amount"><?php echo $slide['top_left']['price']; ?></p>
+					</div><!-- .content -->
+				</div><!-- .featured-product -->
+			<?php endforeach; ?>
+		</div>
+		<div class="featured-top-right">
+			<?php foreach($sliders as $id => $slide): ?>
+				<div class="featured-product text-center featured-image-<?php echo $id; ?>">
+					<div class="image lazy-load">
+						<a href="index.php?page=product-simple"><img class="img-responsive" src="assets/images/blank.gif" data-echo="<?php echo $slide['top_right']['image']; ?>" alt=""></a>
+					</div><!-- .image -->
+					<div class="product-body">
+						<h3 class="product-title"><a href="index.php?page=product-simple"><?php echo $slide['top_right']['title']; ?></a></h3>
+						<p class="product-amount"><?php echo $slide['top_right']['price']; ?></p>
+					</div><!-- .content -->
+				</div><!-- .featured-product -->
+			<?php endforeach; ?>
+		</div>
+	</div><!-- .col -->
+</div>
