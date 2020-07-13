ef59c179c83fa32c113b53757aee7f8f4ef89845
Shahzaib <shahzaibux@gmail.com>
2020-07-14 02:23:10 +0500

    Updated Project

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,40 @@
+<?php
+
+    define( 'ROOT', dirname(__FILE__) );
+
+    $page_name = isset( $_GET['page'] ) ? $_GET['page'] : 'home';
+    $page = ROOT . '/pages/' . $page_name . '.php';
+
+    if( $page_name == 'home' ) {
+        $header = 'header.php';
+        $footer = 'footer.php';
+    } else {
+        $header = ( isset($_GET['header'] ) && $_GET['header'] == 'dark' ) ? 'header-2.php' : 'header.php' ;
+        $footer = ( isset( $_GET['footer'] ) && $_GET['footer'] == '1' ) ? 'footer-2.php' : 'footer.php' ;
+    }
+
+    $header = ROOT . '/parts/' . $header;
+    $footer = ROOT . '/parts/' . $footer;
+
+    $pages = array(
+        'about'             => 'About Us',
+        'adult-services'    => 'Adult Services',
+        'blog'              => 'Blog',
+        'child-refuses'     => 'Child Refuses',
+        'child-services'    => 'child Services',
+        'consultation'      => 'consultation',
+        'contact'           => 'Contact',
+        'corporate-services'=> 'Corporate Services',
+        'family-and-friend' => 'Family and Friend',
+        'family-services'   => 'Family Services',
+        'faqs'              => 'FAQs',
+        'home'              => 'Home',
+        'medico-legal'      => 'Medico Legal',
+        'save-data'         => 'Save Data',
+        'index'             => 'index',
+
+    );
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +50,24 @@
     <!-- https://getbootstrap.com/ -->
     <link rel="stylesheet" href="css/templatemo-style.css">
 </head>
-<body>
+<body id="reportsPage">
+    <div class="" id="home">
+        
+        <?php if($page_name == 'home'): ?>
+            
+        <?php endif; ?>
+
+        <?php
+            if (isset ($_GET['parts'] ) ) {
+                require ROOT . '/parts/' . $_GET['parts'] ;
+            } else {
+                require $header;
+                require $page;
+                require $footer;
+            }
+        ?>
+        
+    </div><!-- /.wrapper -->
     
 
     <script src="js/jquery-3.3.1.min.js"></script>
