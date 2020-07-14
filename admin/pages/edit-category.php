c26e15553bf7cb66ff7627b853301e986755b77e
Shahzaib <shahzaibux@gmail.com>
2020-07-15 04:58:12 +0500

    Updated Project

--- admin/pages/edit-category.php
+++ admin/pages/edit-category.php
@@ -4,7 +4,9 @@
         include "assets/include/config.php";
 
         $categoryid = mysqli_real_escape_string($con,$_POST['$cat_id']);
-        $catname = mysqli_real_escape_string($con,$_POST['cat_name']);
+        $categoryname = mysqli_real_escape_string($con,$_POST['category_name']);
+        $categoryDesc = mysqli_real_escape_string($con,$_POST['category_desc']);
+
         $sql = "UPDATE category SET category_name = '{$catname}' WHERE category_id = '{$categoryid}'";
 
         if(mysqli_query($con, $sql)){
@@ -27,17 +29,21 @@
           <div class="col-xl-6 col-lg-6 col-md-12">
             <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" class="tm-edit-product-form" enctype="multipart/form-data">
               <div class="form-group mb-3">
+                <input id="name" name="cat_id" type="hidden" class="form-control validate" value="<?php echo $row['category_id']; ?>" required />
+              </div>
+              <div class="form-group mb-3">
                 <label for="name" >Category Name </label>
-                <input id="name" name="category_name" type="text" class="form-control validate" required />
+                <input id="name" name="category_name" type="text" class="form-control validate" value="<?php echo $row['category_name']; ?>" required />
               </div>
               <div class="form-group mb-3">
                 <label for="category_desc" >Description</label>
-                <textarea name="category_desc" class="form-control validate" rows="5" required ></textarea>
+                <textarea name="category_desc" class="form-control validate" rows="5" required ><?php echo $row['category_desc'] ?></textarea>
               </div>
           </div>
           <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
             <div class="tm-product-img-dummy mx-auto">
-              <i class="fas fa-cloud-upload-alt tm-upload-icon" onclick="document.getElementById('fileInput').click();" ></i>
+                <img  src="assets/img/<?php echo $row['category_img']; ?>" height="240px">
+                <input type="hidden" name="old_image" value="<?php echo $row['category_img']; ?>">
             </div>
             <div class="custom-file mt-3 mb-3">
               <input id="fileInput" type="file" name="fileToUpload" style="display:none;" />
