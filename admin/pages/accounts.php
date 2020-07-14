75d9703bbb75bcb4804badd9a720bfd0c47fe620
Shahzaib <shahzaibux@gmail.com>
2020-07-14 15:41:14 +0500

    Updated Project

--- admin/pages/accounts.php
+++ admin/pages/accounts.php
@@ -14,12 +14,11 @@
         <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
             <h2 class="tm-block-title">List of Accounts</h2>
             <p class="text-white">Accounts</p>
-            <select class="custom-select">
-            <option value="0">Select account</option>
-            <option value="1">Admin</option>
-            <option value="2">Editor</option>
-            <option value="3">Merchant</option>
-            <option value="4">Customer</option>
+            <select class="custom-select" name="role">
+              <option value="0">Select account</option>
+              <option value="1">Admin</option>
+              <option value="2">Editor</option>
+              <option value="3">Seller</option>
             </select>
         </div>
         </div>
--- admin/pages/add-user.php
+++ admin/pages/add-user.php
@@ -1,26 +1,25 @@
-<?php include "assets/include/header.php"; 
-    
+<?php    
 
     if(isset($_POST['save'])){
-        include "config.php";
+        include "assets/include/config.php";
 
-        $fname = mysqli_real_escape_string($con,$_POST['fname']);
-        $lname = mysqli_real_escape_string($con,$_POST['lname']);
-        $user = mysqli_real_escape_string($con,$_POST['user']);
+        $username = mysqli_real_escape_string($con,$_POST['username']);
+        $email = mysqli_real_escape_string($con,$_POST['email']);
         $password = mysqli_real_escape_string($con,md5($_POST['password']));
+        $phone = mysqli_real_escape_string($con,$_POST['phone']);
         $role = mysqli_real_escape_string($con,$_POST['role']);
 
-    $sql = "SELECT username FROM user WHERE username = '{$user}'";
+    $sql = "SELECT Username FROM user WHERE username = '{$username}'";
     $result = mysqli_query($con,$sql) or die("Query Failed");
 
     if(mysqli_num_rows($result) > 0){
         echo "<p style='color:red;text-align:center;margin: 10px 0;'>Username already Exists</p>";
     }else{
-        $sql1 = "INSERT INTO user (first_name, last_name, username, password, role)
-        VALUES('{$fname}','{$lname}','{$user}','{$password}','{$role}')";
+        $sql1 = "INSERT INTO user (Username, Email, Password, Phone, Role)
+        VALUES('{$username}','{$email}','{$password}','{$phone}','{$role}')";
 
     if(mysqli_query($con,$sql1)){
-        header("location:users.php");
+        header("location: {$hostname}/admin/index.php?page=accounts");
     }
     }
 
@@ -46,10 +45,10 @@
         <div class="tm-block-col tm-col-account-settings">
             <div class="tm-bg-primary-dark tm-block tm-block-settings">
               <h2 class="tm-block-title">Account Settings</h2>
-              <form action="<?php $_SERVER['PHP_SELF']; ?>" class="tm-signup-form row">
+              <form action="<?php $_SERVER['PHP_SELF']; ?>" method ="POST" class="tm-signup-form row">
                 <div class="form-group col-lg-6">
                   <label for="name">Account Name</label>
-                  <input id="name" name="name" type="text" class="form-control validate" />
+                  <input id="name" name="username" type="text" class="form-control validate" />
                 </div>
                 <div class="form-group col-lg-6">
                   <label for="email">Account Email</label>
@@ -69,7 +68,7 @@
                 </div>
                 <div class="form-group col-lg-6">
                   <label for="phone">Account Role</label>
-                  <select class="custom-select">
+                  <select class="custom-select" name="role">
                     <option value="0">Select account</option>
                     <option value="1">Admin</option>
                     <option value="2">Editor</option>
@@ -78,7 +77,7 @@
                 </div>
                 <div class="col-12">
                   <label class="tm-hide-sm">&nbsp;</label>
-                  <button type="submit" class="btn btn-primary btn-block text-uppercase" >
+                  <button type="submit" name="save" class="btn btn-primary btn-block text-uppercase" >
                     Save Profile
                   </button>
                 </div>
