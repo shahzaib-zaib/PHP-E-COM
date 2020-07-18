b8b58800f232dbf4a7b05a299908a9bba280bbcb
Shahzaib <shahzaibux@gmail.com>
2020-07-19 02:11:51 +0500

    Updated Project

--- admin/pages/edit-subcategory.php
+++ admin/pages/edit-subcategory.php
@@ -5,7 +5,7 @@
       <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
         <div class="row">
           <div class="col-12">
-            <h2 class="tm-block-title d-inline-block">Add Category</h2>
+            <h2 class="tm-block-title d-inline-block">Edit Subcategory</h2>
           </div>
         </div>
         <div class="row tm-edit-product-row">
@@ -15,8 +15,8 @@
                 include "../assets/include/config.php";
 
                 $subcat_id = $_GET['id'];
-                $sql = "SELECT category.category_id, category.category_name, category.category_desc, category.category_img
-                 FROM subcategory WHERE sub_id = {$subcat_id}";
+
+                $sql = "SELECT * FROM subcategory  WHERE sub_id = {$subcat_id}";
 
                 $result = mysqli_query($con, $sql) or die ("Query Faild.");
                 if(mysqli_num_rows($result) > 0){
@@ -35,6 +35,12 @@
 
                               if(mysqli_num_rows($result) > 0){
                                   while($row = mysqli_fetch_assoc($result)){
+                                    if ($row['category']  == $row1['category_id']) {
+                                        $selected = "selected";
+                                    } else {
+                                        $selected = "";
+                                    }
+                                    
                                       echo "<option  value='{$row['category_id']}'>{$row['category_name']}</option>";
                                   }
                               }
@@ -43,28 +49,32 @@
                   </div>
                   <div class="form-group mb-3">
                       <label for="name">Subcategory Name</label>
-                      <input id="name" name="subcategory_name" type="text" class="form-control validate" required />
+                      <input id="name" name="sub_id" type="hidden" class="form-control validate" value="<?php echo $row['sub_id']; ?>" required />
+                      <input id="name" name="subcategory_name" type="text" class="form-control validate" value="<?php echo $row['subcategory_name']; ?>" required />
                   </div>
                   <div class="form-group mb-3">
                     <label for="category_desc" >Description</label>
-                    <textarea name="subcategory_desc" class="form-control validate" rows="5" required ></textarea>
+                    <textarea name="subcategory_desc" class="form-control validate" rows="5" value="<?php echo $row['subcategory_desc']; ?>" required ></textarea>
                   </div>
                 </div>
                 <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
                   <div class="tm-product-img-dummy mx-auto">
-                    <i class="fas fa-cloud-upload-alt tm-upload-icon" onclick="document.getElementById('fileInput').click();" ></i>
+                  <img  src="../assets/img/<?php echo $row['subcategory_img']; ?>" height="240px">
+                <input type="hidden" name="old_image" value="<?php echo $row['subcategory_img']; ?>">
                   </div>
                   <div class="custom-file mt-3 mb-3">
-                    <input id="fileInput" type="file" name="fileToUpload" style="display:none;" />
-                    <input type="button" type="file" name="fileToUpload" class="btn btn-primary btn-block mx-auto" value="UPLOAD PRODUCT IMAGE" onclick="document.getElementById('fileInput').click();" />
+                    <input id="fileInput" type="file" name="image" style="display:none;" />
+                    <input type="button" type="hidden" name="old_image" class="btn btn-primary btn-block mx-auto" value="UPLOAD PRODUCT IMAGE" onclick="document.getElementById('fileInput').click();" />
                   </div>
                 </div>
                 <div class="col-12">
-                  <button type="submit" name="save" class="btn btn-primary btn-block text-uppercase">Add Category Now</button>
+                  <button type="submit" name="save" class="btn btn-primary btn-block text-uppercase">Edit subCategory</button>
                 </div>
             </form>
             <?php
                     }
+                }else{
+                    echo "Result Not Found";
                 }
             ?>
         </div>
