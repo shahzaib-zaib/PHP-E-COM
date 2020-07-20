c9c2de7579981c53bd185da75334f0e874376bbf
Shahzaib <shahzaibux@gmail.com>
2020-07-20 09:11:14 +0500

    Updated Project

--- admin/pages/add-product.php
+++ admin/pages/add-product.php
@@ -1,3 +1,39 @@
+<?php
+  
+  if(isset($_POST['submit'])){
+    include "../assets/include/config.php";
+    
+    $category=$_POST['category'];
+    $subcat=$_POST['subcategory'];
+    $productname=$_POST['productName'];
+    $productcompany=$_POST['productCompany'];
+    $productprice=$_POST['productprice'];
+    $productpricebd=$_POST['productpricebd'];
+    $productdescription=$_POST['productDescription'];
+    $productscharge=$_POST['productShippingcharge'];
+    $productavailability=$_POST['productAvailability'];
+    $productimage1=$_FILES["productimage1"]["name"];
+    $productimage2=$_FILES["productimage2"]["name"];
+    $productimage3=$_FILES["productimage3"]["name"];
+  //for getting product id
+  $query=mysqli_query($con,"select max(id) as pid from products");
+    $result=mysqli_fetch_array($query);
+    $productid=$result['pid']+1;
+    $dir="productimages/$productid";
+  if(!is_dir($dir)){
+      mkdir("productimages/".$productid);
+    }
+
+    move_uploaded_file($_FILES["productimage1"]["tmp_name"],"productimages/$productid/".$_FILES["productimage1"]["name"]);
+    move_uploaded_file($_FILES["productimage2"]["tmp_name"],"productimages/$productid/".$_FILES["productimage2"]["name"]);
+    move_uploaded_file($_FILES["productimage3"]["tmp_name"],"productimages/$productid/".$_FILES["productimage3"]["name"]);
+  $sql=mysqli_query($con,"insert into products(category,subCategory,productName,productCompany,productPrice,productDescription,shippingCharge,productAvailability,productImage1,productImage2,productImage3,productPriceBeforeDiscount) values('$category','$subcat','$productname','$productcompany','$productprice','$productdescription','$productscharge','$productavailability','$productimage1','$productimage2','$productimage3','$productpricebd')");
+  $_SESSION['msg']="Product Inserted Successfully !!";
+
+  }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
