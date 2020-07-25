56a0710cdc27313839585c85c262219d3828698d
Shahzaib <shahzaibux@gmail.com>
2020-07-25 15:05:48 +0500

    Updated Project

--- pages/login.php
+++ pages/login.php
@@ -1,60 +1,69 @@
 <?php
-session_start();
-error_reporting(0);
-include('includes/config.php');
-// Code user Registration
-if(isset($_POST['submit']))
-{
-$name=$_POST['fullname'];
-$email=$_POST['emailid'];
-$contactno=$_POST['contactno'];
-$password=md5($_POST['password']);
-$query=mysqli_query($con,"insert into users(name,email,contactno,password) values('$name','$email','$contactno','$password')");
-if($query)
-{
-	echo "<script>alert('You are successfully register');</script>";
-}
-else{
-echo "<script>alert('Not register something went worng');</script>";
-}
-}
-// Code for User login
-if(isset($_POST['login']))
-{
-   $email=$_POST['email'];
-   $password=md5($_POST['password']);
-$query=mysqli_query($con,"SELECT * FROM users WHERE email='$email' and password='$password'");
-$num=mysqli_fetch_array($query);
-if($num>0)
-{
-$extra="my-cart.php";
-$_SESSION['login']=$_POST['email'];
-$_SESSION['id']=$num['id'];
-$_SESSION['username']=$num['name'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=1;
-$log=mysqli_query($con,"insert into userlog(userEmail,userip,status) values('".$_SESSION['login']."','$uip','$status')");
-$host=$_SERVER['HTTP_HOST'];
-$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/$extra");
-exit();
-}
-else
-{
-$extra="login.php";
-$email=$_POST['email'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=0;
-$log=mysqli_query($con,"insert into userlog(userEmail,userip,status) values('$email','$uip','$status')");
-$host  = $_SERVER['HTTP_HOST'];
-$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/$extra");
-$_SESSION['errmsg']="Invalid email id or Password";
-exit();
-}
-}
+    session_start();
+    error_reporting(0);
 
+    include "../include/config.php";
 
+    // Code user Registration
+    if(isset($_POST['submit'])){
+        $name = $_POST['fullname'];
+        $email = $_POST['emailid'];
+        $contactno = $_POST['contactno'];
+        $password = md5($_POST['password']);
+        $sql = "INSERT INTO users(name,email,contactno,password)
+        values('$name','$email','$contactno','$password')";
+        $query = mysqli_query($con, $sql);
+
+        if($query){
+            echo "<script>alert('You are successfully register');</script>";
+        }
+        else{
+            echo "<script>alert('Not register something went worng');</script>";
+        }
+    }
+
+    // Code for User login
+    if(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $sql1 = "SELECT * FROM users WHERE email = '$email' and password = '$password'";
+        $query1 = mysqli_query($con, $sql1);
+        $num = mysqli_fetch_array($query1);
+
+        if($num > 0){
+            $extra = "my-cart.php";
+            $_SESSION['login'] = $_POST['email'];
+            $_SESSION['id'] = $num['id'];
+            $_SESSION['username'] = $num['name'];
+            $uip = $_SERVER['REMOTE_ADDR'];
+            $status = 1;
+
+            $sql2 = "INSERT INTO userlog(userEmail,userip,status) 
+            values('".$_SESSION['login']."','$uip','$status')";
+
+            $log = mysqli_query($con, $sql2);
+            $host = $_SERVER['HTTP_HOST'];
+            $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+            header("location:http://$host$uri/$extra");
+            exit();
+        }
+        else{
+            $extra = "login.php";
+            $email = $_POST['email'];
+            $uip = $_SERVER['REMOTE_ADDR'];
+            $status = 0;
+
+            $sql3 = "INSERT INTO userlog(userEmail,userip,status) 
+            values('$email','$uip','$status')";
+
+            $log = mysqli_query($con, $sql3);
+            $host = $_SERVER['HTTP_HOST'];
+            $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+            header("location:http://$host$uri/$extra");
+            $_SESSION['errmsg']="Invalid email id or Password";
+            exit();
+        }
+    }
 ?>
 
 <script type="text/javascript">
--- pages/my-account.php
+++ pages/my-account.php
@@ -1,42 +1,43 @@
 <?php
-session_start();
-error_reporting(0);
-include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:login.php');
-}
-else{
-	if(isset($_POST['update']))
-	{
-		$name=$_POST['name'];
-		$contactno=$_POST['contactno'];
-		$query=mysqli_query($con,"update users set name='$name',contactno='$contactno' where id='".$_SESSION['id']."'");
-		if($query)
-		{
-echo "<script>alert('Your info has been updated');</script>";
-		}
-	}
+   
+
+    include "include/config.php";
+
+    if(strlen($_SESSION['login'])==0){   
+
+        header("Location: {$hostname}/index.php?page=login.php");
+    }
+    else{
+        if(isset($_POST['update']))
+        {
+            $name=$_POST['name'];
+            $contactno=$_POST['contactno'];
+            $query=mysqli_query($con,"update users set name='$name',contactno='$contactno' where id='".$_SESSION['id']."'");
+            if($query)
+            {
+    echo "<script>alert('Your info has been updated');</script>";
+            }
+        }
 
 
-date_default_timezone_set('Asia/Kolkata');// change according timezone
-$currentTime = date( 'd-m-Y h:i:s A', time () );
+    date_default_timezone_set('Asia/Kolkata');// change according timezone
+    $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 
-if(isset($_POST['submit']))
-{
-$sql=mysqli_query($con,"SELECT password FROM  users where password='".md5($_POST['cpass'])."' && id='".$_SESSION['id']."'");
-$num=mysqli_fetch_array($sql);
-if($num>0)
-{
- $con=mysqli_query($con,"update students set password='".md5($_POST['newpass'])."', updationDate='$currentTime' where id='".$_SESSION['id']."'");
-echo "<script>alert('Password Changed Successfully !!');</script>";
-}
-else
-{
-	echo "<script>alert('Current Password not match !!');</script>";
-}
-}
+    if(isset($_POST['submit']))
+    {
+    $sql=mysqli_query($con,"SELECT password FROM  users where password='".md5($_POST['cpass'])."' && id='".$_SESSION['id']."'");
+    $num=mysqli_fetch_array($sql);
+    if($num>0)
+    {
+    $con=mysqli_query($con,"update students set password='".md5($_POST['newpass'])."', updationDate='$currentTime' where id='".$_SESSION['id']."'");
+    echo "<script>alert('Password Changed Successfully !!');</script>";
+    }
+    else
+    {
+        echo "<script>alert('Current Password not match !!');</script>";
+    }
+    }
 
 ?>
 
