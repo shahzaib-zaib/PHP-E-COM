fcf7285be2badce84282ed51dde4529eb0e3e7ff
Shahzaib <shahzaibux@gmail.com>
2020-07-27 04:40:40 +0500

    Updated Project

--- admin/pages/save-product.php
+++ admin/pages/save-product.php
@@ -15,8 +15,8 @@
     $shippingCharges = mysqli_real_escape_string($con, $_POST['shipping_charges']);
  
 
-    $sql1 = "INSERT INTO products(category,sub_category,product_name,product_company,product_price,sale_price,product_description,product_image1,shipping_charge,product_availability,product_stock,product_image2,product_image3)
-               VALUES('$category', '$subCategory', '$productName', '$productBrand', '$productPrice', '$salePrice', '$productDesc', '$productimage1', '$shippingCharges', '$proAvailb', '$stock','$productimage2','$productimage3');";
+    $sql1 = "INSERT INTO products(category,sub_category,product_name,product_company,product_price,sale_price,product_description,product_image1,shipping_charge,product_availability,product_stock)
+               VALUES('$category', '$subCategory', '$productName', '$productBrand', '$productPrice', '$salePrice', '$productDesc', '$productimage1', '$shippingCharges', '$proAvailb', '$stock');";
 
     $sql .= "UPDATE subcategory SET products = products + 1 WHERE sub_id = {$subCategory}";
       
